dee4af377723991f2b47b1995fd2ae7c7e7b88b0
sanpadhy <santosh.padhy@sapient.com>
2019-05-19 13:20:50 +0530

    Updates

---
+++ precache-manifest.443115ac3c48202cd533e49c9bb73dd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb1dff0d698ac091bb4ed9dbd24196a",
+    "url": "/picAlbum/index.html"
+  },
+  {
+    "revision": "d6810b9b4305c3a2bc2d",
+    "url": "/picAlbum/static/css/main.48990820.chunk.css"
+  },
+  {
+    "revision": "2ada7bafce20d14f0bcf",
+    "url": "/picAlbum/static/js/2.7029a9be.chunk.js"
+  },
+  {
+    "revision": "d6810b9b4305c3a2bc2d",
+    "url": "/picAlbum/static/js/main.586bbe22.chunk.js"
+  },
+  {
+    "revision": "9433f137ac067696c392",
+    "url": "/picAlbum/static/js/runtime~main.89dc6e04.js"
+  }
+]);
